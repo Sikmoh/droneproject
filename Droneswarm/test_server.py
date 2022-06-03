3f4f0e46e219ed0a780f980f9c35828e2a91c94a
Sikmoh <mohsikiru@gmail.com>
2022-06-03 12:03:30 +0000

    update test

--- Droneswarm/test_server.py
+++ Droneswarm/test_server.py
@@ -1,8 +1,5 @@
-import socket
 import threading
 from unittest import TestCase
-
-
 from server import create_server
 from client2 import create_drone
 
@@ -10,8 +7,27 @@ from client2 import create_drone
 class Test(TestCase):
     """ testing the server against the client"""
 
+    def test_server_host(self):
+        gcs = create_server('127.0.0.1', 9999, 1)
+        gcs.create_socket()
+
+        client = create_drone('127.0.0.1', 9999)
+        client.socket_conn()
+        self.assertEqual(gcs.host, '127.0.0.1')
+        client.s.close()
+        gcs.s.close()
+
+    def test_server_port(self):
+        gcs = create_server('127.0.0.1', 9999, 1)
+        gcs.create_socket()
+
+        client = create_drone('127.0.0.1', 9999)
+        client.socket_conn()
+        self.assertEqual(gcs.port, 9999)
+        client.s.close()
+        gcs.s.close()
+
     def test_server_client_sockets(self):
-        # run the server in the background to mimic two different platforms
         gcs = create_server('127.0.0.1', 9999, 1)
         gcs_thread = threading.Thread(target=gcs.create_socket())
         gcs_thread.start()
@@ -19,8 +35,8 @@ class Test(TestCase):
         # this is from the client
         client = create_drone('127.0.0.1', 9999)
         client.socket_conn()
-        assert gcs.host == '127.0.0.1'
-        assert gcs.port == 9999
+        assert gcs.number_of_drones == 1
+        assert len(gcs.all_connections) == 0
         client.s.close()
         gcs_thread.join()
         gcs.s.close()
