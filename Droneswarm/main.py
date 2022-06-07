44b1587bd92fa5d8ba9bdace3f1ee75256889e6f
Sikmoh <mohsikiru@gmail.com>
2022-06-07 13:30:30 +0000

    sever file

---
+++ Droneswarm/main.py
@@ -0,0 +1,18 @@
+#  Module Imports
+from server import create_server
+
+
+#  Setup
+def create_connection(host, port, number):
+    """
+           Function to run the server
+
+       """
+    gcs_server = create_server(host, port, number)
+    gcs_server.create_socket()
+    gcs_server.accept_conn()
+    gcs_server.send_commands()
+
+
+if __name__ == '__main__':
+    create_connection('127.0.0.1', 9999, 1)
